2de62bbbf9a1bcd24a86c5664ed0296ff159ad9a
Keshav Sharma <kassassin99@gmail.com>
2018-12-02 13:34:46 +0530

    Initial commit

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    Vector2 origin;
+
+    [SerializeField]
+    GameObject player;
+
+    void Start () {
+        origin = new Vector2(-8.5f, 1.13f);
+    }
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.GetComponent<PlayerController>().enabled = false;
+            Instantiate(player, origin, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    float speed = 10f;
+
+	void Start () {
+        transform.GetComponent<PlayerController>().enabled = true;
+    }
+	
+	void Update () {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), 0f).normalized;
+        transform.Translate(input * speed * Time.deltaTime);
+	}
+
+   
+}
